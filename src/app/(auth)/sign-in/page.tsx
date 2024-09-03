20702d42c12eba08befdeb72f052630315be2881
shreyanshgangwar1509 <shreyanshgangwar1@gmail.com>
2024-09-03 19:33:33 +0530

    second commit

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,14 @@
-
+import {useDebounceValue} from 'usehooks-ts'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 function page() {
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernaemMEssage] = useState('');
+  const [isCheckingUsername, setIsCHeckingUsername] = useState(false);
+  const [isSubmitting,setIssubmitting] = useState() 
+  const debouncedUsername = useDebounceValue(username, 300);
+  
   return (
     <div>page</div>
   )
